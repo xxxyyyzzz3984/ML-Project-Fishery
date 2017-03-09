20f7ab0e1afca70799156e66457cb8c1facd29d8
yinhaoxiao <xyh3984@gwu.edu>
2017-03-09 16:41:06 -0500

    clean up

--- NoF_Detector/NoF_Detector.py
+++ NoF_Detector/NoF_Detector.py
@@ -25,48 +25,50 @@ def max_pool_3x3(x):
                         strides=[1, 3, 3, 1], padding='SAME')
 
 
-x = tf.placeholder(tf.float32, shape=[None, scan_wnd_size[0] * scan_wnd_size[1], 3])
-y_ = tf.placeholder(tf.float32, [None, 2])
 
-x_image = tf.reshape(x, [-1, scan_wnd_size[0], scan_wnd_size[1], 3], name='image_pnet')
-
-W_conv1 = weight_variable([3, 3, 3, 32], name='wconv1_pnet')
-b_conv1 = bias_variable([32], name='bconv1_pnet')
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_3x3(h_conv1)  ## one layer 3x3 max pooling
-
-W_conv2 = weight_variable([3, 3, 32, 64], name='wconv2_pnet')
-b_conv2 = bias_variable([64], name='bconv2_pnet')
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_3x3(h_conv2)  ## one layer 2x2 max pooling
-
-W_conv3 = weight_variable([3, 3, 64, 64], name='wconv3_pnet')
-b_conv3 = bias_variable([64], name='bconv3_pnet')
-h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
-h_pool3 = max_pool_2x2(h_conv3)  ## one layer 2x2 max pooling
-
-W_conv4 = weight_variable([2, 2, 64, 128], name='wconv4_pnet')
-b_conv4 = bias_variable([128], name='bconv4_pnet')
-h_conv4 = tf.nn.relu(conv2d(h_pool3, W_conv4) + b_conv4)
-
-## fully connected
-W_fc1 = weight_variable([3 * 3 * 128, 256], name='wfc1_pnet')
-b_fc1 = bias_variable([256], name='bfc1_pnet')
-
-h_pool2_flat = tf.reshape(h_conv4, [-1, 3 * 3 * 128])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-
-W_fc2 = weight_variable([256, 2], name='wfc2_pnet')
-b_fc2 = bias_variable([2], name='bfc2_pnet')
-
-
-y_conv = tf.matmul(h_fc1, W_fc2) + b_fc2
 
 def predict_has_fish(test_pic_path):
+    x = tf.placeholder(tf.float32, shape=[None, scan_wnd_size[0] * scan_wnd_size[1], 3])
+    y_ = tf.placeholder(tf.float32, [None, 2])
+
+    x_image = tf.reshape(x, [-1, scan_wnd_size[0], scan_wnd_size[1], 3], name='image_pnet')
+
+    W_conv1 = weight_variable([3, 3, 3, 32], name='wconv1_pnet')
+    b_conv1 = bias_variable([32], name='bconv1_pnet')
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_3x3(h_conv1)  ## one layer 3x3 max pooling
+
+    W_conv2 = weight_variable([3, 3, 32, 64], name='wconv2_pnet')
+    b_conv2 = bias_variable([64], name='bconv2_pnet')
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_3x3(h_conv2)  ## one layer 2x2 max pooling
+
+    W_conv3 = weight_variable([3, 3, 64, 64], name='wconv3_pnet')
+    b_conv3 = bias_variable([64], name='bconv3_pnet')
+    h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
+    h_pool3 = max_pool_2x2(h_conv3)  ## one layer 2x2 max pooling
+
+    W_conv4 = weight_variable([2, 2, 64, 128], name='wconv4_pnet')
+    b_conv4 = bias_variable([128], name='bconv4_pnet')
+    h_conv4 = tf.nn.relu(conv2d(h_pool3, W_conv4) + b_conv4)
+
+    ## fully connected
+    W_fc1 = weight_variable([3 * 3 * 128, 256], name='wfc1_pnet')
+    b_fc1 = bias_variable([256], name='bfc1_pnet')
+
+    h_pool2_flat = tf.reshape(h_conv4, [-1, 3 * 3 * 128])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    W_fc2 = weight_variable([256, 2], name='wfc2_pnet')
+    b_fc2 = bias_variable([2], name='bfc2_pnet')
+
+    y_conv = tf.matmul(h_fc1, W_fc2) + b_fc2
+
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
     with tf.Session() as sess:
         saver = tf.train.Saver()
-        saver.restore(sess,  'NoF_Detector/nof_train.ckpt')
+        saver.restore(sess, 'NoF_Detector/nof_train.ckpt')
         print("Model restored.")
 
         image = io.imread(test_pic_path)
--- find_save_fish.py
+++ find_save_fish.py
@@ -1,54 +1,44 @@
+import argparse
 from os import listdir
 from os.path import join, isfile
 import tensorflow as tf
-import time
-
 from Pretrained_VGG_Fish_Detector.vgg16_fish_detector import find_fish_vgg
-import threading
+
 
 def wrapup_save_fish(image_path, image_name, save_root_dir):
+    print 'Processing image ' + image_name
+    print 'Using VGG Fish Detector'
     test_pic_name = image_name.replace('.jpg', '').replace('.png', '')
     has_fish_vgg = find_fish_vgg(image_path, test_pic_name, save_root_dir)
 
     if not has_fish_vgg:
+        print 'Using NOF Fish Detector'
         from NoF_Detector.NoF_Detector import predict_has_fish
-        has_fish_nof = predict_has_fish(test_pics_folder + test_pic)
+        has_fish_nof = predict_has_fish(image_path)
         tf.reset_default_graph()
 
         if not has_fish_nof:
-            pass
+            print 'NoF does not find fish, skip'
 
         else:
+            print 'Using Onet Fish Detector'
             from Onet_Fish_Detector.Onet_Detector import find_fish_onet
             test_pic_name = image_name.replace('.jpg', '').replace('.png', '')
             find_fish_onet(image_path, test_pic_name, save_root_dir)
+            tf.reset_default_graph()
 
 
 if __name__ == '__main__':
-    test_pics_folder = '../test dataset/'
 
-    test_pics = [f for f in listdir(test_pics_folder)
-                 if isfile(join(test_pics_folder, f))]
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-f', nargs=1)
+
+    opts = ap.parse_args()
+    images_folder = opts.f[0]
+
+    test_pics = [f for f in listdir(images_folder)
+                 if isfile(join(images_folder, f))]
 
     count = 0
-    t = [None, None, None, None, None]
     for test_pic in test_pics:
-        t[count] = threading.Thread(target=wrapup_save_fish,
-            args=(test_pics_folder + test_pic, test_pic, '../find test fish/'))
-
-        t[count].start()
-
-        count += 1
-
-        if count == 5:
-            t[0].join()
-            t[1].join()
-            t[2].join()
-            t[3].join()
-            t[4].join()
-
-            count = 0
-
-
-
-
+        wrapup_save_fish(images_folder + test_pic, test_pic, '../find test fish/')
